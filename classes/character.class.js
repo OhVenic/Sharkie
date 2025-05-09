939bf1ec18728fd6414bb677cb12284bdff095d2
Noah Velickovic <noah.velic@hotmail.com>
2025-05-09 23:10:36 +0200

    walk animation

--- classes/character.class.js
+++ classes/character.class.js
@@ -2,6 +2,8 @@ class Character extends MovableObject {
  
       height = 250; // Set the height of the character
       width = 280; // Set the width of the character
+      speed = 10
+      world;
      IMAGES_WALKING = [
            "img/1.Sharkie/3.Swim/1.png",
            "img/1.Sharkie/3.Swim/2.png",
@@ -20,12 +22,27 @@ class Character extends MovableObject {
 
      animate() {
 
+      setInterval( () => { 
+            if(this.world.keyboard.RIGHT) {
+            this.x += this.speed; // Move the character to the right
+            }
+
+            if(this.world.keyboard.LEFT) {
+                  this.x -= this.speed; // Move the character to the left
+                  }
+      }, 1000 / 25);
+
       setInterval( () => {
+
+            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+ 
+            // Walk animation
             let i = this.currentImage % this.IMAGES_WALKING.length; // Ensure the index is within bounds
             let path = this.IMAGES_WALKING[i];
             this.img = this.imageCache[path];
             this.currentImage++;
-      }, 200);
+            }
+      }, 100);
      }
 
      jump() {
